e57c908fcd5393c61442afc44525f02b9422e685
tinafredriksson <tina.mfredriksson@gmail.com>
2025-10-21 14:29:26 +0200

    created task 2&3

--- js/app.js
+++ js/app.js
@@ -35,6 +35,66 @@ async function getMeals() {
 // Kör funktionen
 getMeals();
 */
+/*
+// Print all meals that contain a given category
+
+async function getMealsByCategory(category) {
+  const letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  let allMeals = [];
+
+  // Hämta alla måltider
+  for (let letter of letters) {
+    const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`);
+    const data = await res.json();
+    if (data.meals) {
+      allMeals = allMeals.concat(data.meals);
+    }
+  }
+  // Filtrera efter given kategori
+  const filteredMeals = allMeals.filter(meal =>
+    meal.strCategory.toLowerCase() === category.toLowerCase()
+  );
+
+  //
+  if (filteredMeals.length === 0) {
+    console.log(`Inga måltider hittades i kategorin: ${category}`);
+  } else {
+    console.log(`Måltider i kategorin "${category}":`);
+    filteredMeals.forEach(meal =>
+      console.log(`${meal.strMeal} (${meal.strCategory})`)
+    );
+  }
+}
+// kör funktionen med given kategori
+getMealsByCategory("Dessert");
+/*
 
 
+ */
+//Create a Javascript object that contains how many times a meals of a each category appears
 
+async function countMealsByCategory() {
+  const letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  let allMeals = [];
+
+  // hämta måltider för varje bokstav
+  for (let letter of letters) {
+    const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`);
+    const data = await res.json();
+    if (data.meals) {
+      allMeals = allMeals.concat(data.meals);
+    }
+  }
+  // reducera till kategorier
+  const categoryCount = allMeals.reduce((acc, meal) => {
+    const category = meal.strCategory;
+    if (!acc[category]) {
+      acc[category] = 1;
+    } else {
+      acc[category]++;
+    }
+    return acc;
+  }, {});
+  console.log(categoryCount);
+}
+countMealsByCategory();
